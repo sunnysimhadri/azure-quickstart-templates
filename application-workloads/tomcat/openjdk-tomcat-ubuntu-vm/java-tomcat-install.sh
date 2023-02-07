b5665f775b3687e1fb338f9f005673f81b8e407c
sunnysimhadri <35062216+sunnysimhadri@users.noreply.github.com>
2023-02-07 16:10:35 +0530

    Update java-tomcat-install.sh

--- application-workloads/tomcat/openjdk-tomcat-ubuntu-vm/java-tomcat-install.sh
+++ application-workloads/tomcat/openjdk-tomcat-ubuntu-vm/java-tomcat-install.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y $1
 sudo apt-get install -y  $2
 
 #install mysql-server 5.6
-sudo apt-get -y install mysql-server-5.6
+sudo apt-get -y install mysql-server-5.6 tree
 
 # Update and then close
 sudo apt-get -y update
